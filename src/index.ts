92844dd7ab0633ad3b574f72f361b9a54181b667
Tyler Liu <tyler4long@gmail.com>
2024-10-06 13:23:39 -0700

    Refactor code

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,20 @@ Split({
   columnGutters: [
     {
       track: 1,
-      element: document.querySelector('.gutter-col-1')!,
+      element: document.querySelector('#col-gutter')!,
     },
   ],
 });
 
-const rowGutter = document.querySelector('.gutter-row-1') as HTMLDivElement;
-const rowsGrid = document.querySelector('.grid-rows') as HTMLDivElement;
+const rowGutter = document.querySelector('#row-gutter') as HTMLDivElement;
+const rowsGrid = document.querySelector('#rows-grid') as HTMLDivElement;
 const toolbar = document.querySelector('#toolbar') as HTMLDivElement;
 rowGutter.addEventListener('click', () => {
   if (toolbar.style.visibility === 'hidden') {
     rowsGrid.style.gridTemplateRows = '16px 8px 1fr';
     toolbar.style.visibility = 'visible';
-    return;
+  } else {
+    rowsGrid.style.gridTemplateRows = '0px 8px 1fr';
+    toolbar.style.visibility = 'hidden';
   }
-  rowsGrid.style.gridTemplateRows = '0px 8px 1fr';
-  toolbar.style.visibility = 'hidden';
 });
