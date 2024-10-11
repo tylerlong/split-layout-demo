fa8d2be6a8166855148e01b90b3d77e3089b8727
Tyler Liu <tyler4long@gmail.com>
2024-10-10 21:09:06 -0700

    Fix a scroll issue

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,19 @@ const rowsGrid = document.querySelector('#rows-grid') as HTMLDivElement;
 const toolbar = document.querySelector('#toolbar') as HTMLDivElement;
 rowGutter.addEventListener('click', () => {
   rowsGrid.style.gridTemplateRows =
-    toolbar.offsetHeight === 0 ? '24px 8px 1fr' : '0px 8px 1fr';
+    toolbar.offsetHeight === 0 ? '24px 6px 1fr' : '0px 6px 1fr';
   rowGutter.title =
     toolbar.offsetHeight === 0 ? 'Show toolbar' : 'Hide toolbar';
 });
+
+// how to programmatically change the panel width
+// const colsGrid = document.querySelector('#cols-grid') as HTMLDivElement;
+// let count = 0;
+// setInterval(() => {
+//   count += 1;
+//   if (count % 2 === 0) {
+//     colsGrid.style.gridTemplateColumns = '2fr 6px 1fr';
+//   } else {
+//     colsGrid.style.gridTemplateColumns = '1fr 6px 2fr';
+//   }
+// }, 3000);
